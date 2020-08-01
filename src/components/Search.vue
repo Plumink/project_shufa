8371e86fb657a7d2b10e0fb7856944fb65a74d3f
zhangxintaobb <1309526743@qq.com>
2020-08-01 17:51:56 +0800

    show

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,15 +1,57 @@
 <template>
-  <div>
-      
+  <div id="search" class>
+    <form class="search_from" :opacity="0.2">
+      <input type="text" placeholder="请输入您要搜索的内容..." class="search_input" />
+      <button type="submit" class="search_button">search</button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      transparency: 0,
+    };
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+#search {
+  width: 100%;
+  height: 30%;
+  background-image: url("../../images/search_background.jpg");
+  background-size: 100% 100%;
+  position: relative;
+  opacity: 0.6;
+}
+.search_from {
+  width: 80%;
+  height: 20%;
+  opacity: 0.6;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 10%;
+}
+.search_input {
+  width: 80%;
+  height: 100%;
+  border: 2px solid #7ba7ab;
+  border-radius: 5px;
+  background: #f9f0da;
+  color: #9e9c9c;
+}
+.search_button {
+  height: 100%;
+  width: 20%;
+  top: 0;
+  right: 0;
+  background: #7ba7ab;
+  border-radius: 0 5px 5px 0;
+}
 </style>
--- src/views/study/Show.vue
+++ src/views/study/Show.vue
@@ -1,21 +1,48 @@
 <template>
   <v-app>
-      <FootNavigation />
-      <TopNavigation />
+    <FootNavigation />
+    <TopNavigation />
+    <Search />
+    <div class="d-flex flex-row justify-space-around box">
+      <div class="box-left">
+          <img src="../../assets/logo.png" alt="">
+      </div>
+      <div class="d-flex flex-column justify-space-around box-right">
+          <div class=""></div>
+      </div>
+    </div>
   </v-app>
 </template>
 
 <script>
-import FootNavigation from '../../components/FootNavigation'
-import TopNavigation from '../../components/TopNavigation'
+import FootNavigation from "../../components/FootNavigation";
+import TopNavigation from "../../components/TopNavigation";
+import Search from "../../components/Search";
 export default {
-    components:{
-        FootNavigation,
-        TopNavigation
-    }
-}
+  components: {
+    FootNavigation,
+    TopNavigation,
+    Search,
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.box {
+  width: 100%;
+  height: 90px;
+}
+.box-left {
+  width: 25%;
+  padding: 20px;
+}
+.box-right {
+  width: 75%;
+  background-color: blue;
+  padding: 10px;
+}
+.box-left img{
+    width: 100%;
+    height: 100%;
+}
 </style>
