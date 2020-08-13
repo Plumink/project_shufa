2313b9e9d6a25eec0103132ba5f8fcb94519ef9f
lvshufeng520 <1002463595@qq.com>
2020-08-13 09:35:14 +0800

    add generate

---
+++ src/views/home/Generate.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="total_gen">
+    <TopNavigation />
+    <img src="" alt="">
+  </div>
+</template>
+
+<script>
+import FootNavigation from "../../components/FootNavigation";
+import TopNavigation from "../../components/TopNavigation";
+export default {
+  components: {
+    FootNavigation,
+    TopNavigation
+  },
+  data() {
+    return {
+      ziti: this.$route.query.word
+    }
+  },
+  methods: {
+    
+  }
+
+}
+</script>
+
+<style scoped>
+  /* @import "./style.css"; */
+  .total_gen {
+    width: 100vw;
+    height: 100vh;
+    background-color: #F9F4E6;
+  }
+
+</style>
