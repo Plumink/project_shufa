6ee654968d55ffca9790554a1a85bccf574c38ec
lvshufeng520 <1002463595@qq.com>
2020-08-24 09:40:43 +0800

    new style

--- src/views/home/Generate.vue
+++ src/views/home/Generate.vue
@@ -1,9 +1,17 @@
 <template>
   <v-app>
     <div class="total_gen">
-      <TopNavigation />
+      <!-- <TopNavigation /> -->
+      
+      <div style="width:100vw;height:10vh;position: fixed;z-index: 1000;background-color:#f9f4e6">
+        <i class="iconfont icon-back icon-style" @click="backHome()"></i>
+        <el-button type="primary" style="float:right;margin-top:2vh;margin-right:4vw">分享作品</el-button>
+      </div>
       <div style="height:10vh;width:100vw"></div>
-      <div style="width:100vw;height:10vh;background-color:red"></div>
+      <div class="sec_input">
+        <span>标题</span>
+        <el-input v-model="input" placeholder="请输入内容" style="width:60%;margin-left:2vw"></el-input>
+      </div>
       <div style="background-color:#f9f4e6;width:100vw;height:auto">
         <div class="d-flex flex-row-reverse justify-space-around" style="margin:0 auto;width:80%;height:80%">
           <div class="d-flex flex-column mb-6" v-for="(items,index) in show" :key="index" :style="{ width: width+ '%', height:height+'%' }">
@@ -17,7 +25,6 @@
           </div>
         </div>
       </div>
-      <div style="width:100vw;height:20vh;background-color:red"></div>
     </div>
   </v-app>
 </template>
@@ -35,10 +42,15 @@ export default {
       data: [],
       show: [],
       width:'0',
-      height:'0'
+      height:'0',
+      input:''
     };
   },
-  methods: {},
+  methods: {
+    backHome() {
+      this.$router.push('/homelogin')
+    }
+  },
   created() {
     var str = this.$route.query.message.text;
     var arr = str.match(/[\u4e00-\u9fa5]/g);
@@ -92,5 +104,19 @@ export default {
   width: 100vw;
   height: 100vh;
   background-color: #f9f4e6;
+}
+
+.icon-style {
+  font-size: 7vw;
+  color: grey;
+  line-height: 10vh;
+  margin-left: 4vw;
+}
+
+.sec_input {
+  width: 100vw;
+  height: 10vh;
+  margin: 0 auto;
+  text-align: center;
 }
 </style>
