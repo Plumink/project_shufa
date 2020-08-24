cd10d8dae80176cbb6b00c7a68c60a9ea40391bc
zhangxintaobb <1309526743@qq.com>
2020-08-24 16:14:22 +0800

    release

--- src/views/person/Follower.vue
+++ src/views/person/Follower.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
-    <div class="follower_all" v-for="(item,index) in items" :key="index">
-      <img src="https://s1.ax1x.com/2020/08/04/aDBQL8.jpg" alt="">
-      <div style="padding-top:2vh;">
-        <span class="ziti_follower" style="margin-left:2vw;">用户名：</span>
-        <br/>
-        <div style="width:20vw;float:left;">
-          <span class="ziti_follower" style="margin-left:2vw;">关注数：</span>
+    <div class="d-flex flex-row justify-space-around box" v-for="(item,index) in item" :key="index" @click="jump(item.number)">
+      <div class="box-left">
+        <img :src="item.image" alt />
+      </div>
+      <div class="d-flex flex-column justify-space-around box-right">
+        <div class="d-flex flex-row justify-start align-center">
+          <span style="font-size:14px">{{item.number}}号 ：</span>
+          <h4 class="show_title">{{item.title | title}}</h4>
         </div>
-        <div style="float:left">
-          <span class="ziti_follower" style="margin-left:8vw;">粉丝数：</span>
-        </div>
+        <span style="font-size:14px;margin:0;color:#BDBDBD;">{{item.content | content}}</span>
       </div>
     </div>
   </div>
@@ -20,16 +19,89 @@
 export default {
   data(){
     return{
-      items: [1,2,3,4,5,6]
+      item: [
+        {
+          image: "https://s1.ax1x.com/2020/07/31/alrgsK.th.png",
+          number: "70162",
+          title: "李商隐《锦瑟》草书版",
+          name: "吕峰",
+          read: "0",
+          content:
+            "锦瑟无端五十弦，一弦一柱思华年。庄生晓梦迷蝴蝶，望帝春心托杜鹃",
+        },
+        {
+          image: "https://s1.ax1x.com/2020/07/31/alrgsK.th.png",
+          number: "70162",
+          title: "李商隐《锦瑟》草书版",
+          name: "吕峰",
+          read: "0",
+          content:
+            "锦瑟无端五十弦，一弦一柱思华年。庄生晓梦迷蝴蝶，望帝春心托杜鹃",
+        },
+        {
+          image: "https://s1.ax1x.com/2020/07/31/alrgsK.th.png",
+          number: "70162",
+          title: "李商隐《锦瑟》草书版",
+          name: "吕峰",
+          read: "0",
+          content:
+            "锦瑟无端五十弦，一弦一柱思华年。庄生晓梦迷蝴蝶，望帝春心托杜鹃",
+        },
+        {
+          image: "https://s1.ax1x.com/2020/07/31/alrgsK.th.png",
+          number: "70162",
+          title: "李商隐《锦瑟》草书版",
+          name: "吕峰",
+          read: "0",
+          content:
+            "锦瑟无端五十弦，一弦一柱思华年。庄生晓梦迷蝴蝶，望帝春心托杜鹃",
+        },
+        {
+          image: "https://s1.ax1x.com/2020/07/31/alrgsK.th.png",
+          number: "70162",
+          title: "李商隐《锦瑟》草书版",
+          name: "吕峰",
+          read: "0",
+          content:
+            "锦瑟无端五十弦，一弦一柱思华年。庄生晓梦迷蝴蝶，望帝春心托杜鹃",
+        },
+        {
+          image: "https://s1.ax1x.com/2020/07/31/alrgsK.th.png",
+          number: "70162",
+          title: "李商隐《锦瑟》草书版",
+          name: "吕峰",
+          read: "0",
+          content:
+            "锦瑟无端五十弦，一弦一柱思华年。庄生晓梦迷蝴蝶，望帝春心托杜鹃",
+        },
+      ],
     }
   },
   methods:{
-
-  }
+    jump(id){
+      this.$router.push({ path: '/show/info' });
+    }
+  },
+   filters: {
+    title(value) {
+      if (!value) return "";
+      if (value.length > 8) {
+        return value.slice(0, 8) + "...";
+      }
+      return value;
+    },
+    content(value) {
+      if (!value) return "";
+      if (value.length > 8) {
+        return value.slice(0, 14) + "...";
+      }
+      return value;
+    },
+  },
 }
 </script>
 
-<style>
+<style scoped>
   .follower_all {
     width: 100%;
     height: 13vh;
@@ -51,4 +123,25 @@ export default {
     font-size: 3vw;
     font-family:YouYuan;
   }
+#show {
+  width: 100%;
+  height: 100%;
+}
+.box {
+  width: 100%;
+  height: 90px;
+  border-bottom: 1px solid #00838f;
+}
+.box-left {
+  width: 25%;
+  padding: 20px;
+}
+.box-right {
+  width: 75%;
+  padding: 10px;
+}
+.box-left img {
+  width: 100%;
+  height: 100%;
+}
 </style>
