13382b7991fa70af1189ceb591c8b56a88e9a9f6
zhangxintaobb <1309526743@qq.com>
2020-09-11 15:17:46 +0800

    fix bug

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-<div id="app">
-  <router-view />
-</div>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: "app", 
+  name: "app",
   data: () => ({
     //
   }),
-  created: function(){
+  created: function () {
     var code;
     var url = document.URL;
     var that = this;
@@ -30,76 +29,78 @@ export default {
         that.openid = res.data.data.openid;
         var id = res.data.data.openid;
         that.setCookie("openid", id, 360);
+        this.$axios
+          .post(
+            "/CalligraphyService/user/getUserInfo",
+            {
+              openId: id,
+            },
+            {
+              headers: { "X-Request-ID": "1" },
+            }
+          )
+          .then((response) => {
+            console.log(response);
+            var userData = {
+              custId: response.data.data.custId,
+              custTel: response.data.data.custTel,
+              custName: response.data.data.custName,
+              custPass: response.data.data.custPass,
+              custImgHead: response.data.data.custPass,
+              tag: response.data.data.tag,
+              custLastTime: response.data.data.custLastTime,
+              vipEffDate: response.data.data.vipEffDate,
+              openid: response.data.data.openId,
+              isVip: response.data.data.isVip,
+              vipExpDate: response.data.data.vipExpDate,
+              invalidTime: response.data.data.invalidTime,
+              ifValid: response.data.data.ifValid,
+            };
+            this.$store.commit("wechatLogin", userData);
+          });
       });
     // this.setCookie("openid",this.openid,360);
-    var openid=this.getCookie('openid')
-    this.$axios.post('/CalligraphyService/user/getUserInfo',
-    {
-      "openId": openid
-    },
-    {
-      headers:{'X-Request-ID':'1'}
-    }
-    )
-    .then((response)=>{
-      console.log(response)
-      var userData={
-        custId:response.data.data.custId,
-        custTel:response.data.data.custTel,
-        custName:response.data.data.custName,
-        custPass:response.data.data.custPass,
-        custImgHead:response.data.data.custPass,
-        tag:response.data.data.tag,
-        custLastTime:response.data.data.custLastTime,
-        vipEffDate:response.data.data.vipEffDate,
-        openid:response.data.data.openId,
-        isVip:response.data.data.isVip,
-        vipExpDate:response.data.data.vipExpDate,
-        invalidTime:response.data.data.invalidTime,
-        ifValid:response.data.data.ifValid
-      }
-      this.$store.commit('wechatLogin',userData)
-    })
-    if(this.$store.state.land==true || openid!=''){
-      if(this.$route.path=='/main'){
+
+    if (this.$store.state.land == true || this.$store.state.openid != "") {
+      if (this.$route.path == "/main") {
         this.$router.push(
-            {path: '/main'},
-            onComplete => {},
-            onAbort => {}
-         )
-      }
-      else if(this.$route.path=='/main/follow' ){
+          { path: "/main" },
+          (onComplete) => {},
+          (onAbort) => {}
+        );
+      } else if (this.$route.path == "/main/follow") {
         this.$router.push(
-            {path: '/main/follow'},
-            onComplete => {},
-            onAbort => {}
-         )
-      }
-      else if(this.$route.path=='/main/follower'){
+          { path: "/main/follow" },
+          (onComplete) => {},
+          (onAbort) => {}
+        );
+      } else if (this.$route.path == "/main/follower") {
         this.$router.push(
-            {path: '/main/follower'},
-            onComplete => {},
-            onAbort => {}
-         )
-      }
-      else{
+          { path: "/main/follower" },
+          (onComplete) => {},
+          (onAbort) => {}
+        );
+      } else {
         this.$router.push(
-            {path: '/homelogin'},
-            onComplete => {},
-            onAbort => {}
-         )
+          { path: "/homelogin" },
+          (onComplete) => {},
+          (onAbort) => {}
+        );
       }
-    }
-    else{
-      if(this.$route.path=='/main' || this.$route.path=='/main/follow' || this.$route.path=='/main/follower' ){
+    } else {
+      if (
+        this.$route.path == "/main" ||
+        this.$route.path == "/main/follow" ||
+        this.$route.path == "/main/follower"
+      ) {
         this.$message.error("尚未登陆");
         this.$router.push(
-            {path: '/'},
-            onComplete => {},
-            onAbort => {}
-         )
+          { path: "/" },
+          (onComplete) => {},
+          (onAbort) => {}
+        );
       }
     }
-  }
-}
+  },
+};
 </script>
