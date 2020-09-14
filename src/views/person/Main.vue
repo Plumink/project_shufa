a02afe5769d55a3fde6e7d2049446480ea1e72f2
zhangxintaobb <1309526743@qq.com>
2020-09-14 10:55:48 +0800

    debug

--- src/views/person/Main.vue
+++ src/views/person/Main.vue
@@ -205,22 +205,21 @@ export default {
                 }
               }
             );
-
           }
-          // if (typeof WeixinJSBridge == "undefined") {
-          //   if (document.addEventListener) {
-          //     document.addEventListener(
-          //       "WeixinJSBridgeReady",
-          //       onBridgeReady,
-          //       false
-          //     );
-          //   } else if (document.attachEvent) {
-          //     document.attachEvent("WeixinJSBridgeReady", onBridgeReady);
-          //     document.attachEvent("onWeixinJSBridgeReady", onBridgeReady);
-          //   }
-          // } else {
+          if (typeof WeixinJSBridge == "undefined") {
+            if (document.addEventListener) {
+              document.addEventListener(
+                "WeixinJSBridgeReady",
+                onBridgeReady,
+                false
+              );
+            } else if (document.attachEvent) {
+              document.attachEvent("WeixinJSBridgeReady", onBridgeReady);
+              document.attachEvent("onWeixinJSBridgeReady", onBridgeReady);
+            }
+          } else {
             onBridgeReady(this);
-          // }
+          }
         });
     },
   },
